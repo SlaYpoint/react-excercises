9abde3ae5f0aae650c777a9acbaec27abeead963
SlaYpoint <66507607+SlaYpoint@users.noreply.github.com>
2021-04-20 20:01:48 +0530

    added show/hide

--- src/components/PasswordMatch.js
+++ src/components/PasswordMatch.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 const PasswordMatch = () => {
-  let [pswd, setPswd] = useState("");
-  let [msg, showMsg] = useState(" ");
-  let [btnDisable, setDisable] = useState(true);
+  const [pswd, setPswd] = useState("");
+  const [msg, showMsg] = useState(" ");
+  const [btnDisable, setDisable] = useState(true);
+  const [passType, setType] = useState("password");
+  const [show, setShow] = useState("Show");
 
   const checkAlphaNum = (pass) => {
     let digit = /\d/;
@@ -16,14 +18,23 @@ const PasswordMatch = () => {
     }
   };
 
+  const hideHandler = () => {
+    if (passType === "password") {
+      setType("text");
+      setShow("Hide");
+    } else {
+      setType("password");
+      setShow("Show");
+    }
+  };
   return (
     <div>
       <h2>Password Matching</h2>
-      <input type="password" onChange={(e) => checkAlphaNum(e.target.value)} />
+      <input type={passType} onChange={(e) => checkAlphaNum(e.target.value)} />
       <br />
       <br />
       <input
-        type="password"
+        type={passType}
         onChange={(e) => {
           let confirm = e.target.value;
 
@@ -37,8 +48,11 @@ const PasswordMatch = () => {
       />
       <br />
       <br />
-      <p>{msg}</p>
+      <p> {msg} </p>
+      <small onClick={hideHandler}> {show} </small>
+
       <button disabled={btnDisable} onClick={console.log(pswd)}>
+        {" "}
         Submit
       </button>
     </div>
