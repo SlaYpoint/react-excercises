e5adb84c1524e282876846509a758c5819781f55
SlaYpoint <66507607+SlaYpoint@users.noreply.github.com>
2021-08-04 22:38:55 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 import { Counter } from './components/Counter';
 import CharCounter from './components/CharCounter';
 import PasswordMatch from './components/PasswordMatch';
@@ -12,6 +13,34 @@ import ToDo from './components/ToDo/ToDo';
 export default function App() {
 	return (
 		<div className="App">
+			<nav className="navbar">
+				<ul className="navlink">
+					<li>
+						<Link to="/">Router</Link>
+					</li>
+					<li>
+						<Link to="/counter">Counter</Link>
+					</li>
+					<li>
+						<Link to="/charcounter">Char Counter</Link>
+					</li>
+					<li>
+						<Link to="/password">Password Match</Link>
+					</li>
+					<li>
+						<Link to="/cart">Shopping Cart</Link>
+					</li>
+					<li>
+						<Link to="/toast">Toast</Link>
+					</li>
+					<li>
+						<Link to="/darkmode">Dark Mode</Link>
+					</li>
+					<li>
+						<Link to="/todo">ToDo</Link>
+					</li>
+				</ul>
+			</nav>
 			{/* <Counter /> */}
 			{/* <CharCounter /> */}
 			{/* <PasswordMatch /> */}
@@ -19,7 +48,31 @@ export default function App() {
 			{/* <SwitchTabs /> */}
 			{/* <Toast /> */}
 			{/* <DarkMode /> */}
-			<ToDo />
+			{/* <ToDo /> */}
+			<Route exact path="/">
+				<SwitchTabs />
+			</Route>
+			<Route path="/counter">
+				<Counter />
+			</Route>
+			<Route path="/charcounter">
+				<CharCounter />
+			</Route>
+			<Route path="/password">
+				<PasswordMatch />
+			</Route>
+			<Route path="/cart">
+				<ShoppingCart />
+			</Route>
+			<Route path="/toast">
+				<Toast />
+			</Route>
+			<Route path="/darkmode">
+				<DarkMode />
+			</Route>
+			<Route path="/todo">
+				<ToDo />
+			</Route>
 		</div>
 	);
 }
